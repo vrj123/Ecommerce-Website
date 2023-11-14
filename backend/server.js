66486cd4213104d1688e876d945053df541f3292
Vijay <javijay876@gmail.com>
2023-11-14 08:52:41 +0530

    Set up the backend

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const app=require('./app');
+
+
+process.on('uncaughtException', (err)=>{
+    console.log(`Error: ${err.message}`);
+    console.log("shutting down the server for handling uncaught exceptions");
+})
+
+if (process.env.NODE_ENV !== "PRODUCTION") {
+    require("dotenv").config({
+      path: "backend/config/.env",
+    });
+  }
+
+  const server=app.listen(process.env.PORT, ()=>{
+    console.log(`Sever started on port number ${process.env.PORT}`)
+  })
+  
+  process.on('unhandledRejection', (err)=>{
+    console.log(`Shutting down the server for ${err.message}`);
+    console.log('Shutting dowm the server for promise rejection');
+  
+    server.close(()=>{
+      process.exit(1);
+    });
+  })
