5d3ff06f7ad69484be19875f1d0a3c2da1d7e42e
Vijay <javijay876@gmail.com>
2023-11-15 10:10:36 +0530

    Database connection created and created middlewares for error handling

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const app=require('./app');
+const connectDatabase = require('./db/Database');
 
 
 process.on('uncaughtException', (err)=>{
@@ -6,11 +7,15 @@ process.on('uncaughtException', (err)=>{
     console.log("shutting down the server for handling uncaught exceptions");
 })
 
+// config
 if (process.env.NODE_ENV !== "PRODUCTION") {
     require("dotenv").config({
       path: "backend/config/.env",
     });
   }
+
+// connect database
+connectDatabase();
 
   const server=app.listen(process.env.PORT, ()=>{
     console.log(`Sever started on port number ${process.env.PORT}`)
