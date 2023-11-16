04c4df22618b683eb7c4f518a3ff1505adc07a15
Vijay <javijay876@gmail.com>
2023-11-16 13:09:58 +0530

    Added user model and controller for create user

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,15 @@
 const express=require('express');
+const ErrorHandler = require('./utils/ErrorHandler');
 const app=express();
+const cookieParser=require('cookie-parser');
+const bodyParser=require('body-parser');
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/', express.static('uploads'));
+app.use(bodyParser.urlencoded({extended:true}));
+
 
 
 
@@ -11,5 +21,14 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
     });
   }
 
+
+  // import routes
+  const user=require('./controller/user');
+
+  app.use('/api/v2/user', user);
+
+
+// it is for error handling
+app.use(ErrorHandler);
 
 module.exports = app;
