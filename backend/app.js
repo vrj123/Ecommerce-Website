306be3476b7e87b79fdf6c8c73f3261df3f9008b
Vijay <javijay876@gmail.com>
2024-01-31 12:56:35 +0530

    incresed limit for parsing json data

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(cors({
   origin:['https://ecommerce-website-8uam.vercel.app'],
   credentials:true,
 }));
-app.use(express.json());
+app.use(express.json({limit:'50mb'}));
 app.use(cookieParser());
 app.use('/test', (req, res)=>{
   res.send('hello World')
